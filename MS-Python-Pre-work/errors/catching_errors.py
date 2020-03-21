3d01df737cfeafc4f2badbebcb7790956742f986
Daniel Nakolah <oladidi.daniel@gmail.com>
2020-03-21 16:41:25 +0300

    catching errors

---
+++ MS-Python-Pre-work/errors/catching_errors.py
@@ -0,0 +1,9 @@
+def get_age():
+    print("How old are you? ")
+    try:
+        age = int(input())
+        return age
+    except ValueError:
+        return "That was not a valid input"
+
+print(get_age())
