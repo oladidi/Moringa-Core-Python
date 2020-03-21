a4be900ff738e960ee733bef610f9bd86e9d3cad
Daniel Nakolah <oladidi.daniel@gmail.com>
2020-03-21 16:41:40 +0300

    feature_phones

--- MS-Python-Pre-work/list_and_dictionaries/feature_phone.py
+++ MS-Python-Pre-work/list_and_dictionaries/feature_phone.py
@@ -1,1 +1,11 @@
 # This program counts the number clicks a user has to make to write a character in a feature phone
+user_input = str(input('Enter the text: ').lower().strip())
+
+text_input = ''
+for character in user_input:
+    if character.isalnum():
+        text_input += character
+
+keypad = {1: [], 2: ['a', 'b', 'c']}
+
+print(user_input)
